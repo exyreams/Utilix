43fa9ef41f63f09093f39ef37f0db177bad4d7bd
Exyreams <43289621+exyreams@users.noreply.github.com>
2024-08-20 17:10:23 +0000

    new: updated qr generator

--- src/tools/qr_code_generator.rs
+++ src/tools/qr_code_generator.rs
@@ -1,28 +1,50 @@
+use image::Luma;
 use qrcode::render::unicode;
 use qrcode::QrCode;
+use std::path::Path;
 
 pub struct QRCodeGenerator {
     pub input: String,
-    pub qr_code: String,
+    pub qr_code: Option<QrCode>,
 }
 
 impl QRCodeGenerator {
     pub fn new() -> Self {
         QRCodeGenerator {
             input: String::new(),
-            qr_code: String::new(),
+            qr_code: None,
         }
     }
 
     pub fn generate_qr_code(&mut self) {
-        if let Ok(code) = QrCode::new(self.input.as_bytes()) {
-            self.qr_code = code
-                .render::<unicode::Dense1x2>()
-                .dark_color(unicode::Dense1x2::Light)
-                .light_color(unicode::Dense1x2::Dark)
-                .build();
+        self.qr_code = QrCode::new(self.input.as_bytes()).ok();
+    }
+
+    pub fn export_qr_code(&self) -> Result<(), Box<dyn std::error::Error>> {
+        if let Some(qr_code) = &self.qr_code {
+            let export_dir = "export/";
+
+            std::fs::create_dir_all(export_dir)?;
+
+            let truncated_input = self.input.chars().take(10).collect::<String>();
+            let filename = format!("{}.png", truncated_input.replace(" ", "_").to_lowercase());
+
+            let filepath = Path::new(export_dir).join(filename);
+
+            let image = qr_code.render::<Luma<u8>>().build();
+
+            image.save(filepath)?;
+
+            Ok(())
         } else {
-            self.qr_code = "Error generating QR code".to_string();
+            Err("QR code has not been generated yet".into())
+        }
+    }
+
+    pub fn get_qr_string(&self) -> String {
+        match &self.qr_code {
+            Some(qr) => qr.render::<unicode::Dense1x2>().quiet_zone(false).build(),
+            None => String::from("No QR code generated yet"),
         }
     }
 }
