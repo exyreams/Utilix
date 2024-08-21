4446b9df4d8dbd51f79d6c0fea817a450ed03838
Exyreams <43289621+exyreams@users.noreply.github.com>
2024-08-21 12:56:35 +0000

    updated: message on the output field

--- src/tools/qr_code_generator.rs
+++ src/tools/qr_code_generator.rs
@@ -44,7 +44,9 @@ impl QRCodeGenerator {
     pub fn get_qr_string(&self) -> String {
         match &self.qr_code {
             Some(qr) => qr.render::<unicode::Dense1x2>().quiet_zone(false).build(),
-            None => String::from("No QR code generated yet"),
+            None => String::from(
+                "No QR code generated yet, Please enter the data in the input field to create one.",
+            ),
         }
     }
 }
